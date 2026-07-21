497f47bb4a3a084f3cf0a9dacaa0769eb0115d20
raj <ilu.raghus@icloud.com>
2026-07-21 03:59:27 -0600

    check linting using the ci

--- main.go
+++ main.go
@@ -24,11 +24,6 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func unused() {
-	//dOes nothing here. 
-	//also not called anywhere. 
-}
-
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
